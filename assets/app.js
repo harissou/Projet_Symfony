aa648c43afb5137f9e2405c78a51abb2b2748ec1
Harissou <haris.stifler@gmail.com>
2021-12-08 12:36:13 +0100

    Modif asset

--- assets/app.js
+++ assets/app.js
@@ -11,5 +11,7 @@ import './styles/app.scss';
 //you can specify which plugins you need
 import { Tooltip, Toast, Popover } from 'bootstrap';
 
+
+
 // start the Stimulus application
 import './bootstrap';
